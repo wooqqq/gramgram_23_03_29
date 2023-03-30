fa08748310616cbe2f56ac0ac91aaabe15ea4ee6
깃wooqqq <sge0225@naver.com>
2023-03-30 13:56:51 +0900

    회원가입 처리

--- src/main/java/com/ll/gramgram/GramgramApplication.java
+++ src/main/java/com/ll/gramgram/GramgramApplication.java
@@ -2,12 +2,13 @@ package com.ll.gramgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GramgramApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GramgramApplication.class, args);
 	}
-
 }
--- src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram.boundedContext.member.controller;
 
+import com.ll.gramgram.boundedContext.member.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
     @GetMapping("/join")
     public String showJoin() {
         return "usr/member/join";
@@ -33,6 +36,8 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(@Valid JoinForm joinForm) {
+        memberService.join(joinForm.getUsername(), joinForm.getPassword());
+
         return "redirect:/";
     }
 }
